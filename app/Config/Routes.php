21f080e41956451acf7cc52ef1fa066e9e4b1c54
efraim urel <efraimurelpalodang@gmail.com>
2025-01-12 06:35:42 +0800

    baru mencoba membuat controller dan methodnya saja

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('coba/index', 'Coba::index');
+$routes->get('coba/about', 'Coba::about');
---
+++ app/Controllers/Coba.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        echo 'ini controller coba method index';
+    }
+    public function about()
+    {
+        echo 'Hallo nama saya efraim urel palodang';
+    }
+}
