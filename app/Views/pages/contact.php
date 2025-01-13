da56f27e9af9c26d8d0d4674de20720f8989b048
efraim urel <efraimurelpalodang@gmail.com>
2025-01-14 05:50:43 +0800

    memperbagus tampilan dari contact menggunakan table bootstrap

--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -5,13 +5,27 @@
   <div class="row">
     <div class="col">
       <h2>Contact Us</h2>
-      <?php foreach($alamat as $a) : ?>
-        <ul>
-          <li><?= $a['tipe']; ?></li>
-          <li><?= $a['alamat']; ?></li>
-          <li><?= $a['kota']; ?></li>
-        </ul>
-      <?php endforeach; ?>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">tipe</th>
+            <th scope="col">alamat</th>
+            <th scope="col">kota</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $i = 1 ?>
+          <?php foreach($alamat as $a) : ?>
+            <tr>
+              <th scope="row"><?= $i++; ?></th>
+              <td><?= $a['tipe']; ?></td>
+              <td><?= $a['alamat']; ?></td>
+              <td><?= $a['kota']; ?></td>
+            </tr>
+          <?php endforeach; ?>
+          </tbody>
+        </table>
     </div>
   </div>
 </div>
