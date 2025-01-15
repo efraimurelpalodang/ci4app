54b6806d83b9a56688d002f9688518ee4c75e1f9
efraim urel <efraimurelpalodang@gmail.com>
2025-01-16 06:24:14 +0800

    mengubah tampilan untuk menampilkan daftar komiknya

--- app/Views/comic/index.php
+++ app/Views/comic/index.php
@@ -5,27 +5,18 @@
   <div class="row">
     <div class="col">
       <h2 class="my-3">Daftar Komik</h2>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">sampul</th>
-            <th scope="col">judul</th>
-            <th scope="col">aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php $i=1; ?>
-          <?php foreach($komik as $k) : ?>
-          <tr>
-            <th scope="row"><?= $i++; ?></th>
-            <td><img src="/img/<?= $k['sampul']; ?>" alt="gambarSampul" class="sampul"></td>
-            <td><?= $k['judul']; ?></td>
-            <td><a href="#" class="btn btn-success">Detail</a></td>
-          </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
+      <div class="d-flex gap-2 row">
+        <?php foreach($komik as $k) : ?>
+        <div class="card">
+          <img src="/img/<?= $k["sampul"]; ?>" class="card-img-top sampul" alt="sampul col-sm-3">
+          <div class="card-body">
+            <h5 class="card-title"><?= $k["judul"]; ?></h5>
+            <p class="card-text"><?= $k["penulis"]; ?></p>
+            <a href="#" class="btn btn-primary">Detail</a>
+          </div>
+        </div>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
 </div>
