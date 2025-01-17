b34e72a150cca9b19af54f49279df45553662e0c
efraim urel <efraimurelpalodang@gmail.com>
2025-01-18 06:07:17 +0800

    mengubah tampilan agar lebih bagus

--- app/Views/komik/index.php
+++ app/Views/komik/index.php
@@ -2,22 +2,19 @@
 
 <?= $this->section('content'); ?>
 <div class="container">
-  <div class="row">
-    <div class="col">
-      <h2 class="my-3">Daftar Komik</h2>
-      <div class="d-flex gap-2 row">
-        <?php foreach($komik as $k) : ?>
-        <div class="card">
-          <img src="/img/<?= $k["sampul"]; ?>" class="card-img-top sampul" alt="sampul col-sm-3">
-          <div class="card-body">
-            <h5 class="card-title"><?= $k["judul"]; ?></h5>
-            <p class="card-text"><?= $k["penulis"]; ?></p>
-            <a href="/komik/<?= $k["slug"]; ?>" class="btn btn-primary">Detail</a>
+  <h2 class="my-3">Daftar Komik</h2>
+  <div class="d-flex gap-3">
+    <?php foreach( $komik as $k ) : ?>
+      <div class="card kartu position-relative">
+        <a href="komik/<?= $k['slug']; ?>">
+          <img src="/img/<?= $k['sampul']; ?>" alt="sampul"  class="card-img gambar">
+          <div class="card-img-overlay">
+            <h5 class="card-title judul"><?= $k['judul']; ?></h5>
+            <p class="card-text penulis"><small>Last updated 3 mins ago</small></p>
           </div>
-        </div>
-        <?php endforeach; ?>
+        </a>
       </div>
-    </div>
+    <?php endforeach; ?>
   </div>
 </div>
 <?= $this->endSection(); ?>
--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
   <div class="container">
-    <a class="navbar-brand" href="#">Efraim urel palodang</a>
+    <a class="navbar-brand fw-semibold fs-3" href="#">Efraim urel palodang</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/">Home</a>
-        <a class="nav-link" href="about">About</a>
-        <a class="nav-link" href="contact">Contact</a>
-        <a class="nav-link" href="komik">Komik</a>
+      <div class="navbar-nav ms-5">
+        <a class="nav-link active" aria-current="page" href="/">BERANDA</a>
+        <a class="nav-link" href="about">TENTANG KAMI</a>
+        <a class="nav-link" href="contact">KONTAK</a>
+        <a class="nav-link" href="komik">KOMIK</a>
       </div>
     </div>
   </div>
